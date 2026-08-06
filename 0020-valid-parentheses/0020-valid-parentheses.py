216aab3fe8acf3950552c9cf14208bccc1596811
Feri <hosseinbigdeli335@gmail.com>
2026-08-06 16:40:39 +0330

    Time: 0 ms (100%), Space: 12.6 MB (38.67%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isValid(self, s):
+        close_to_open = {")": "(", "]": "[", "}": "{"}
+        stack = []
+        for char in s:
+            if char in close_to_open: 
+                if stack and stack[-1] == close_to_open[char]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(char)
+        return len(stack) == 0      
+
